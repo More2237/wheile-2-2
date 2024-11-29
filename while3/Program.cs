07036e8950d60ca9d32ca82dff7352e610522e89
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-29 23:41:08 +0500

    nbjkl

---
+++ while3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace while3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int K = int.Parse(Console.ReadLine());
+            var result = N;
+            double count = 0;
+
+            while (result>= K)
+            {
+                result -= K;
+                count++;
+                
+            }
+            Console.WriteLine(result);
+            Console.WriteLine(count);
+        }
+    }
+}
